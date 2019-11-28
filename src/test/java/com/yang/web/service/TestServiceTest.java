2ae2f02fdaf35153a092ff041b2c124eadec9577
xmyyzw <xmyyzw@sina.com>
2019-11-28 14:06:40 +0800

    junit2

--- src/test/java/com/yang/web/service/TestServiceTest.java
+++ src/test/java/com/yang/web/service/TestServiceTest.java
@@ -1,16 +1,20 @@
 package com.yang.web.service;
 
+import com.yang.web.config.WebMvcConfig;
 import com.yang.web.pojo.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.List;
 
+@ContextConfiguration(classes = WebMvcConfig.class)
 public class TestServiceTest {
 
     @Autowired
     private FirstService firstService;
 
-    @org.junit.Test
+    @Test
     public void createRecord(){
         List<User> userList = firstService.findUser();
         for (User users: userList
